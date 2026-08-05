ed31bba8b21e5e09f379be93d6e2542bf1ab2763
oualid-choukrallah98 <ochoukrallah@gmail.com>
2026-08-05 12:35:47 +0200

    Add: sql-common-table-expressions - submission-4

---
+++ SQL/sql-common-table-expressions/submission-4.sql
@@ -0,0 +1,54 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+WITH lakwaza AS(
+  SELECT customer_id, MAX(price) as zmla
+  FROM orders
+  GROUP BY customer_id
+  HAVING MAX(price) < 100
+
+)
+
+SELECT c.name
+FROM customers c
+JOIN lakwaza
+ON c.id = lakwaza.customer_id
+ORDER BY c.name ASC ;
+
+
+
+
+
+
+
+
+
